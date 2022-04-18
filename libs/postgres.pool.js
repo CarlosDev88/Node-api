62dc95f5f67f07e093d9203efb00bcc3390b0a44
Carlos <arturorueda88@gmail.com>
2022-04-18 15:50:53 -0500

    intento de trabajar con variables de entorno

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,24 @@
 const { Pool } = require('pg');
+// const { config } = require('../config/config.js');
+
+// const USER = encodeURIComponent(config.dbUser);
+// const PASSWORD = encodeURIComponent(config.dbPassword);
+// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   port: 5432,
+//   user: 'carlos',
+//   password: 'admin123',
+//   database: 'my_store',
+// });
+
+const USER = encodeURIComponent('carlos');
+const PASSWORD = encodeURIComponent('admin123');
+const URI = `postgres://${USER}:${PASSWORD}@localhost:5432/my_store`;
 
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'carlos',
-  password: 'admin123',
-  database: 'my_store',
+  connectionString: URI,
 });
 
 module.exports = pool;
