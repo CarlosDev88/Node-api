8f96a1327b608b44f9938809ece8381583ea0f91
Carlos <arturorueda88@gmail.com>
2022-04-18 14:29:18 -0500

    conexios usando pool

--- services/product.service.js
+++ services/product.service.js
@@ -1,10 +1,14 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
+const pool = require('../libs/postgres.pool');
+
 class ProductService {
   constructor() {
     this.products = [];
     this.generateProduct();
+    this.pool = pool;
+    this.pool.on('error', (err) => console.error(err));
   }
 
   generateProduct() {
@@ -31,11 +35,9 @@ class ProductService {
   }
 
   async find() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(this.products);
-      }, 500);
-    });
+    const query = 'SELECT * FROM tasks';
+    const rta = await this.pool.query(query);
+    return rta.rows;
   }
 
   async findOne(id) {
